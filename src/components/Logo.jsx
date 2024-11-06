b5590b8faebf3a2d1a38d90e93e7b119182e5e52
Chidozie Ukaigwe <chido@Chidozies-MacBook-Air.local>
2024-11-06 19:55:09 +0000

    desktop nav updated with links and styles

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 export default function Logo() {
   return (
-    <div>Logo</div>
+    <Link href="/">
+      <Image src='/next.svg' width={54} height={54} priority alt="" />
+    </Link>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { motion } from "framer-motion"
+const links = [
+    {path: '/', name: 'home'},
+    {path: '/projects', name: 'my projects'},
+    {path: '/contact', name: 'contact'}
+]
+
+export default function Nav({containerStyles, linkStyles, underlineStyles}) {
+    const path = usePathname();
+
+  return (
+    <nav className={`${containerStyles}`}>
+        {links.map((link, index) => (
+            <Link href={link.path} key={index} className={`capitalize ${linkStyles}`}>
+                {link.path === path && (
+                    <motion.span initial={{y: '-100%'}} animate={{y:0}} transition={{type: 'tween'}} layoutId="underline" className={`${underlineStyles}`}/>
+                )}
+                {link.name}
+            </Link>
+        ))}
+    </nav>
+  )
+}
