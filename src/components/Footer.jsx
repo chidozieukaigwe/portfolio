79a874b0f797a8e6f4390f479bb29f8f1036b5a0
Chidozie Ukaigwe <chido@Chidozies-Air.home>
2024-11-09 11:40:07 +0000

    footer added and cta added to app and homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
-
+import Socials from "./Socials"
 
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto px-10">
+        <div className="flex flex-col items-center justify-between">
+          {/* socials */}
+          <Socials  containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4" iconStyles="text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all"/>
+          {/* copyright */}
+          <div className="text-muted-foreground">
+            Copyright &copy; Chido Ukaigwe. All rights reserved.
+          </div>
+        </div>
+      </div>
+
+    </footer>
   )
 }
